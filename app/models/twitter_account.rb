b4febb9f3531ccb18958f41986b7035b246e7b26
Michael Parris <michaelparris@Michaels-MacBook-Pro.local>
2014-06-21 03:24:06 -0700

    Module issue with heroku

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -16,11 +16,11 @@ class TwitterAccount < ActiveRecord::Base
   end
 
   def self.create_for_artist(artist)
-    fetched_account      = TwitterApi::Methods.get_account_for_username(artist.name)
+    # fetched_account      = TwitterApi::Methods.get_account_for_username(artist.name)
     fetched_account_info = ApiParamsExtractor::Twitter.extract_account_info(fetched_account)
     twitter_account      = create(fetched_account_info.merge(:artist_id => artist.id)
 
-    found_tweets = TwitterApi::Methods.tweets_for_account(twitter_account)
+    # found_tweets = TwitterApi::Methods.tweets_for_account(twitter_account)
     found_tweets.each do |tweet|
       tweet_info = ApiParamsExtractor::Twitter.extract_tweet_info(tweet)
       Tweet.create(tweet_info.merge(:twitter_account_id => twitter_account.id))
@@ -28,14 +28,14 @@ class TwitterAccount < ActiveRecord::Base
   end
 
   def update_info
-    fetched_account      = TwitterApi::Methods.get_account_for_username(screen_name)
+    # fetched_account      = TwitterApi::Methods.get_account_for_username(screen_name)
     fetched_account_info = ApiParamsExtractor::Twitter.extract_account_info(fetched_account)
     # if nothing has changed for the account (Same Tweets Count Or Something. Maybe Updated at) don't need to update
     self.update_attributes(fetched_account_info)
 
     # Wipe out existing?
     # Only grab new ones? Don't know how tweets are updated
-    found_tweets = TwitterApi::Methods.tweets_for_account(self, self.tweets.min_twitter_id)
+    # found_tweets = TwitterApi::Methods.tweets_for_account(self, self.tweets.min_twitter_id)
     found_tweets.reject!{|tweet| Tweet.where(:twitter_id => tweet.id)}      
     found_tweets.each do |tweet|
       tweet_info = ApiParamsExtractor::Twitter.extract_tweet_info(tweet)
