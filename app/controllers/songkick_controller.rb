b8aa03876d7f61e54ed0d93c48b9cf97cacf0f47
Michael Parris <michaelparris@Michaels-MacBook-Pro.local>
2014-06-25 01:22:44 -0700

    Forgot some new files

---
+++ app/controllers/songkick_controller.rb
@@ -0,0 +1,7 @@
+class SongkickController < ApplicationController
+
+  def get_shows_for_artist
+    
+  end
+
+end
---
+++ config/initializers/load_api_keys.rb
@@ -0,0 +1,14 @@
+API_KEYS = YAML.load_file(Rails.root.join('config', 'api_keys.yml'))
+
+# Twitter API Keys
+TWITTER_API_KEY    = API_KEYS['twitter']['api_key']
+TWITTER_API_SECRET = API_KEYS['twitter']['api_secret']
+
+# Facebook API Keys
+
+
+#Souncloud API Keys
+SOUNDCLOUD_API_KEY = API_KEYS['souncloud']['api_key']
+
+#Songkick API Keys
+SONGKICK_API_KEY = API_KEYS['songkick']['api_key']
